cc55bb4f1d36196a174ac497ea86104d7e50d8dc
Carlo Angelo Hilaus <carloangelo.ch@gmail.com>
2025-07-16 23:54:34 +0800

    feat(components): added required and disabled attributes for AnimInput

--- frontend/src/components/AnimInput.tsx
+++ frontend/src/components/AnimInput.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
 
@@ -8,8 +8,13 @@ type AnimInputType = {
   type: string;
   placeholder: string;
   maxLength?: number;
-  onKeyDown: (e: React.KeyboardEvent<HTMLInputElement>) => void;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  required?: boolean;
+  disabled?: boolean;
+  onKeyDown?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+  onChange: (
+    e: React.ChangeEvent<HTMLInputElement>,
+    ...args: (string | number | boolean)[]
+  ) => void;
 };
 
 const AnimInput = ({
@@ -18,30 +23,51 @@ const AnimInput = ({
   type,
   placeholder,
   maxLength,
+  required = false,
+  disabled = false,
   onKeyDown,
   onChange,
 }: AnimInputType) => {
   const [showPassword, setShowPassword] = useState(false);
 
+  const refer = useRef<HTMLInputElement>(null);
+
+  const handleClick = () => {
+    if (refer.current) {
+      refer.current.focus();
+    }
+  };
+
   return (
     <>
       <div id="form-input" className="relative w-full mt-6">
         <input
+          ref={refer}
           type={
             type === "password" ? (showPassword ? "text" : "password") : type
           }
           name={name}
-          className="peer bg-white text-black p-1 w-full rounded-lg outline-slate-200 outline-1 outline-offset-1 focus:outline-brand-purple placeholder:text-white"
+          className={`peer ${
+            disabled ? "bg-slate-200 text-slate-500" : "bg-white text-black"
+          } p-1 w-full rounded-lg outline-slate-200 outline-1 outline-offset-1 focus:outline-brand-purple placeholder:text-white`}
           placeholder={placeholder}
           maxLength={maxLength}
           onKeyDown={onKeyDown}
           onChange={onChange}
+          required={required}
+          disabled={disabled}
         />
         <label
           htmlFor={name}
           className="absolute left-1 -top-5 duration-300 ease-in-out text-brand-purple  text-xs peer-placeholder-shown:top-1 peer-placeholder-shown:text-base peer-placeholder-shown:text-slate-400 peer-focus:-top-5 peer-focus:text-xs peer-focus:text-brand-purple"
+          onClick={handleClick}
         >
           {content}
+          {required && (
+            <span className="text-red-400">
+              <strong>*</strong>
+            </span>
+          )}
         </label>
         {type === "password" && (
           <div

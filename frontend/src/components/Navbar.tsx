a33c7f310502bd1cac284e36e6286a75054170e7
Carlo Angelo Hilaus <carloangelo.ch@gmail.com>
2025-07-17 23:56:50 +0800

    feat(navbar): Added UI to Navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,36 +1,51 @@
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/auth";
+import BloopTaskLogo from "./BloopTaskLogo";
+
 const Navbar = () => {
   const { mutate: logout } = useLogout();
 
   const navlinks = [
     {
-      name: "Home",
+      name: "🏠 Home",
       link: "/home",
     },
     {
-      name: "Dashboard",
+      name: "🧮 Dashboard",
       link: "/home/dashboard",
     },
     {
-      name: "Settings",
+      name: "⚙ Settings",
       link: "/home/settings",
     },
   ];
 
   return (
-    <div className="w-full flex flex-col">
-      {navlinks.map((n) => {
-        return (
-          <Link to={n.link} key={n.name} className="btn btn-ghost">
-            {n.name}
-          </Link>
-        );
-      })}
-      <button className="btn btn-error" onClick={() => logout()}>
-        Logout
-      </button>
-    </div>
+    <>
+      <div className="w-full bg-gradient-to-br from-brand-blue/70 via-brand-blue/70 to-brand-purple/90 absolute h-screen -top-5 -left-5 flex flex-col justify-between p-5">
+        <div className="flex flex-col items-center gap-y-4 overflow-y-auto">
+          <div className="w-full flex flex-row items-center gap-x-2">
+            <BloopTaskLogo className="w-10" />
+            <span> BloopTask</span>
+          </div>
+          {navlinks.map((n) => {
+            return (
+              <div className="w-full p-2 rounded-md hover:bg-brand-purple/80 duration-200 hover:drop-shadow-lg ease-in-out hover:pl-4">
+                <Link key={n.name} to={n.link} className="w-full">
+                  {n.name}
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+        <button
+          className="btn btn-error text-white hover:bg-red-600"
+          onClick={() => logout()}
+        >
+          Logout
+        </button>
+      </div>
+    </>
   );
 };
 
